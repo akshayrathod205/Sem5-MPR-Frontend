997fee513526a5c29b87eeeaca1903724ec38523
Divya Shah <ds3636005@gmail.com>
2023-08-30 22:26:43 +0530

    Changed Signup.jsx

--- src/Components/signup.jsx
+++ src/Components/signup.jsx
@@ -15,27 +15,57 @@ const Content = () => {
     function handleClick() {
         navigate("/Details")
     }
+    const [selectedType, setSelectedType] = useState(''); // State to keep track of selected type
+
+    const handleChange=(e) =>{
+        const getValue = e.target.value
+        console.log(getValue)
+        if(getValue===2){
+            const showDetails ={
+                
+            }
+        }
+    }
 
     return (
         <>
             {/* Content */}
             <div className={`container ${isSignUpActive ? 'right-panel-active' : ''}`}>
                 <div className="form-container sign-up-container">
-                    <form action="#">
+                    <form action="/">
                         <h1 className='h1'>Create Account</h1>
-                        <input type="text" placeholder="Name" className='input'/>
+                        <input type="text" placeholder="Name" className='input' />
                         <input type="email" placeholder="Email" className='input' />
-                        <input type="password" placeholder="Password" className='input'/>
-                        <input type="tel" placeholder="Contact Number" pattern='[0-9]{4}[0-9]{3}[0-9]{3}' className='input'/>
-                        <button onClick={handleClick} className='button'>Next</button>
+                        <input type="password" placeholder="Password" className='input' />
+                        <input type="tel" placeholder="Contact Number" pattern='[0-9]{4}[0-9]{3}[0-9]{3}' className='input' />
+                        <textarea className='input' placeholder='Address' name="address" id="" cols="30" rows="2"></textarea>
+                        <div className='inputfield my-1'>
+                            <input className='input-new' type="pincode" placeholder='Pincode' />
+                            <input className='input-new' type="city" placeholder='City' />
+                        </div>
+                        <select className="type-box my-1" aria-label="Default select example" onChange={(e) => handleChange(e)}>
+                            <option disabled selected>Type</option>
+                            <option value="1">Induvidual</option>
+                            <option value="2">Company</option>
+                        </select>
+                        <div className="company-details my-2">
+                            <input type="company-name" className="input-company" placeholder='Company Name' />
+                            <input type="type" className="input-company" placeholder='Company type' />{/* To be changed*/}
+                        </div>
+                        <div className="company-details">
+                            <input type="gst_no" className="input-company" placeholder='Gst No.' />
+                            <input type="pan_no" className="input-company" placeholder='PAN No.' />
+                        </div>
+
+                        <button onClick={handleClick} className='button my-2'>Sign Up</button>
                     </form>
                 </div>
                 <div className="form-container sign-in-container">
-                    <form action="#">
+                    <form action="/">
                         <h1 className='h1'>Sign In</h1>
-                        <input type="email" placeholder="Email" className='input'/>
-                        <input type="password" placeholder="Password" className='input'/>
-                        <a href="#" className='a'>Forgot your password?</a>
+                        <input type="email" placeholder="Email" className='input' />
+                        <input type="password" placeholder="Password" className='input' />
+                        <a href="/" className='a'>Forgot your password?</a>
                         <button className='button'>Sign In</button>
                     </form>
                 </div>
