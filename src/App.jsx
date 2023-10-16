aa5e6d9f6a71df1022f1b6b6747e568e780e72e6
Archie Shah <103347688+Archiesachin@users.noreply.github.com>
2023-10-17 01:08:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
-import { useState } from "react";
-import Navbar from "./Components/navbar";
-import Home from "./Components/landing";
-import Footer from "./Components/footer";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from 'react'
+
+import './App.css'
+import Navbar from './Components/navbar'
+import Landing from './Components/landing'
+import Footer from './Components/footer'
+import {BrowserRouter} from 'react-router-dom'
+import { Route, Routes } from "react-router-dom"
+import Rout from './Components/rout'
 import Signup from './Components/signup'
 import Product from './Components/products'
+import Singleproduct from './Components/singleproduct'
 
 function App() {
+
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Signup />} />
-        <Route path="/products" element={<Product />} />
+    <Navbar />
+    <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path="/log_in" element={<Signup/>}/> 
+        <Route path="/products" element={<Product />}/> 
+        <Route path="/products/:id" element={<Singleproduct />} />
       </Routes>
-      <Footer />
+    <Footer/>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
