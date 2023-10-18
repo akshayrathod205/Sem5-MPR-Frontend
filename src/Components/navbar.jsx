d65a464eed3d8230cdbf272317db6c21f4fa3676
Divya Shah <102017379+divyashah0510@users.noreply.github.com>
2023-10-18 21:16:24 +0530

    Update navbar.jsx

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,47 +1,206 @@
-import "./nav.css";
-import { Link } from "react-router-dom";
-import "react-slideshow-image/dist/styles.css";
-import { Slide } from "react-slideshow-image";
+import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import { styled } from '@mui/material/styles';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export default function Navbar() {
-  return (
-    <>
-      <div className="nav-container">
-        <div className="navbar">
-          <div className="logo">
-            <img src="/logo.png" />
-          </div>
-          <div className="nav-list">
-            <ul>
-              <li>
-                <Link to="/" className="link">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link to="/about" className="link">
-                  About
-                </Link>
-              </li>
-              <li>
-                <Link to="/products" className="link">
-                  Products
-                </Link>
-              </li>
-              <li>
-                <Link to="/contact" className="link">
-                  Contact Us
-                </Link>
-              </li>
-              <button>
-                <Link to="/login" className="btn">
-                  Log In
-                </Link>
-              </button>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+
+const pages = ['Home', 'About Us', 'Products', 'Contact Us'];
+const settings = ['Admin Login', 'User login'];
+
+const Navbar = (props) => {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+    const navigate = useNavigate(
+
+    );
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = (page, index) => {
+        console.log('Clicked on ', index)
+        const pageUrls = {
+            'Home': '/',
+            'About Us': '/about_us',
+            'Products': '/products',
+            'User Login':'/login',
+            'Contact Us': '/contact_us',
+            'Admin login': '/admin_login',
+        }
+        const url = pageUrls[page]
+        if (url) {
+            window.location.href = url;
+        }
+    };
+
+    const handleCloseUserMenu = (setting) => {
+        setAnchorElUser(null);
+
+        // Define the URLs for Admin Login and User Login
+        const loginUrls = {
+            'Admin Login': '/admin_login',
+            'User Login': '/login',
+        };
+
+        // Redirect to the selected login page
+        const url = loginUrls[setting];
+        if (url) {
+            // Use react-router Link to navigate within your React app
+            navigate(url);
+        }
+    };
+    const [style, setstyle] = useState({
+        backgroundColor: '#172c6f',
+        color: 'white'
+    })
+    const [isBlackMode, setIsBlackMode] = useState(false);
+
+
+    return (
+        <AppBar position="relative" style={style}>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        FIT RIGHT
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pages.map((page, index) => (
+                                <MenuItem key={page} onClick={() => { handleCloseNavMenu(page, index) }}>
+                                    <Typography textAlign="center">{page}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'flex', md: 'none' },
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        FIT RIGHT
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map((page, index) => (
+                            <Button
+                                key={page}
+                                onClick={() => handleCloseNavMenu(page, index)}
+                                sx={{ my: 2, color: 'white', display: 'block' }}
+                            >
+                                {page}
+                            </Button>
+                        ))}
+                    </Box>
+                    <Box sx={{ flexGrow: 0 }}>
+                        <Tooltip title="Open settings">
+                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0, mx: 2 }}>
+                                <Avatar alt="Icon Button" src={IconButton} />
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{ mt: '5px' }}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={() => handleCloseUserMenu('')} // Close the menu without redirecting if no option is selected
+                        >
+                            {settings.map((setting) => (
+                                <MenuItem key={setting} onClick={() => handleCloseUserMenu(setting)}>
+                                    <Typography textAlign="center">{setting}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
 }
+
+export default Navbar
+
+
+
+// For running material ui u need to first install dependencies.
+// npm install @mui/material @emotion/react @emotion/styled
+// npm install @mui/material @mui/styled-engine-sc styled-components
+// npm install @mui/icons-material
